c3b9d0a6f653b7bb74c2b705acb4df24c4525165
Zipho1345 <xabanozipho72@gmail.com>
2023-06-17 11:44:50 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - Prints the alphabet without q and e.
- *
+ * main -  Prints the alphabet without q and e.*
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,11 +11,12 @@ int main(void)
 
 	for (i = 97; i < 123; i++)
 	{
-	if (i != 101 && i != 113)
-		  {
-		  putchar(i);
-		  }
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
