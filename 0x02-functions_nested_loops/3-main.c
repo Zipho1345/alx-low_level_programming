4d7b7bda2c4d8d2f00502e503a59e3f3409f3989
Zipho1345 <xabanozipho72@gmail.com>
2023-06-20 08:55:37 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,12 +9,11 @@ int main(void)
 {
 	int t;
 
-
 	r = _islower('H');
 	_putchar(r + '0');
 	r = _islower('o');
 	_putchar(r + '0');
-	r = _islower('n');
+	r = _islower(108);
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
